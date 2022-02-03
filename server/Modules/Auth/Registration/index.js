1a46bd204ad5a988eb58baefdf97bc97d05c7e29
Hrishabh <42819233+hrishabh8871@users.noreply.github.com>
2022-02-03 11:16:59 +0530

    Create index.js

---
+++ server/Modules/Auth/Registration/index.js
@@ -0,0 +1,9 @@
+const { ValidMobileStep1 } = require('./ValidMobileStep1')
+const { ValidEmailStep2 } = require('./ValidEmailStep2')
+const { ValidPAN } = require('./validPAN')
+
+module.exports = {
+    ValidMobileStep1: ValidMobileStep1,
+    ValidEmailStep2: ValidEmailStep2,
+    ValidPAN: ValidPAN
+}
